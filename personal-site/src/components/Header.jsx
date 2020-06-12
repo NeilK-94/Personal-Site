da570465ab955698770c27c5a74527c4527a9686
NeilK-94 <neil.kyne@gmail.com>
2020-06-12 18:27:42 +0100

    Started on header, did styling for selfie but nothing else yet

--- personal-site/src/components/Header.jsx
+++ personal-site/src/components/Header.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
-
+import selfie from "../assets/profilepic.jpg";
 export default class Header extends Component {
     render() {
         return (
-            <div>
-                Header
+            <div className="Header">
+                <div>
+                <img src={selfie} className="Header-Selfie" alt="selfie" />
+                  <div>
+                    <h1>Neil Kyne</h1>
+                    <h2>Web Developer</h2>
+                  </div>
+                </div>
+                <p>
+                I'm a recent Software Developerer graduate from GMIT. Feel free to check out some of my past projects below,
+                browse some code on my github profile, or just reach out to say hi! 
+                </p>
+                
+
+
+
             </div>
         )
     }
